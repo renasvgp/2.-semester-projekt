2676cc54013dc32e9021699a06afcd300c5430b7
renasvgp <100416810+renasvgp@users.noreply.github.com>
2022-12-17 21:40:35 +0100

    Add files via upload

---
+++ Login.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjektNYYYZealand.Pages.Account
+{
+	public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> onPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (Credential.UserName == "admin" && Credential.Password == "password")
+            {
+                var claims = new List<Claim> {
+
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@minHjemmeside.com")
+                };
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "Brugernavn")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
